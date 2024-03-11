9f50a21052c302df7769c9b01f8386cb2dd9094c
Sahil Hussain <mdsahil37621@gmail.com>
2024-03-11 19:14:10 +0530

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 import Projects from './pages/Projects';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/footer.jsx'
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -17,6 +19,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
